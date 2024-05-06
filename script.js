b17c465ca9fb7378b90bf3fee9d0386304e62db5
Ajay kandibedala <119519349+Ajaykandibedala@users.noreply.github.com>
2024-05-06 12:39:59 +0530

    Sign-in website

---
+++ script.js
@@ -0,0 +1,11 @@
+let wrapper = document.querySelector(".wrapper"),
+    button = document.querySelector("header button"),
+    signIn = document.querySelector(".form #sign-in"),
+    signUp = document.querySelector(".form #sign-up"),
+    icon = document.querySelector(".wrapper #close");
+
+signUp.addEventListener("click", () => wrapper.classList.add("active"));
+signIn.addEventListener("click", () => wrapper.classList.remove("active"));
+
+button.addEventListener("click", () => wrapper.classList.add("popup"));
+icon.addEventListener("click", () => wrapper.classList.remove("popup"));
